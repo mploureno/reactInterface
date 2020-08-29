7e66fe011196b16e80f22b98e366353d1af64b12
mp <mploureno@gmail.com>
2020-08-29 17:02:58 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>pepe e nene</p>
+    </div>
+  );
+}
+
+export default App;
