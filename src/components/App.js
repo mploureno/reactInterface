1ab0f9ed2be5ac2ea02343404d4a7b90e0f69fdc
mp <mploureno@gmail.com>
2020-08-31 19:44:21 -0300

    Add basic css

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,18 @@ class App extends Component {
   render() {
     return (
       <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
     );
   }  
 }
