47ae6756ecbf453723341807e41eb235ecf77559
mp <mploureno@gmail.com>
2020-09-05 18:10:40 -0300

    Add delete functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import '../css/App.css';
 import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
+import { without } from 'lodash';
 
 class App extends Component {
   constructor() {
@@ -11,7 +12,17 @@ class App extends Component {
       myAppointments: [],
       lastIndex: 0
     };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
   }
+
+deleteAppointment(apt) {
+  let tempApts = this.state.myAppointments;
+  tempApts = without(tempApts, apt);
+  
+  this.setState({
+    myAppointments: tempApts
+  });
+}
 
   componentDidMount() {
     fetch('./data.json')
@@ -37,7 +48,10 @@ class App extends Component {
               <div className="container">
                 <AddAppointments />
                 <SearchAppointments />
-                <ListAppointments appointments={this.state.myAppointments} />
+                <ListAppointments
+                  appointments={this.state.myAppointments}
+                  deleteAppointment={this.deleteAppointment}
+                />
               </div>
             </div>
           </div>
