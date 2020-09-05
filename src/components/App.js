37526d5b242a7ccc7868a3cd31ec3203f9adbf1c
mp <mploureno@gmail.com>
2020-09-05 18:26:14 -0300

    Create add apointment form

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     super();
     this.state = {
       myAppointments: [],
+      formDisplay: false,
       lastIndex: 0
     };
     this.deleteAppointment = this.deleteAppointment.bind(this);
@@ -46,7 +47,7 @@ deleteAppointment(apt) {
           <div className="row">
             <div className="col-md-12 bg-white">
               <div className="container">
-                <AddAppointments />
+                <AddAppointments formDisplay={this.state.formDisplay} />
                 <SearchAppointments />
                 <ListAppointments
                   appointments={this.state.myAppointments}
